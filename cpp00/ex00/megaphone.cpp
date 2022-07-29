3a1a752999721c914ada193a0c4858a329320fc6
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-29 09:54:31 +0900

    [mod] Makefile

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,3 +1,5 @@
+
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -25,9 +27,9 @@ int	main(int argc, char const *argv[])
 		for (i = 0; i < argc; i++)
 		{
 			std::string str(argv[i]);
-			for (j = 0; j < str.length(); j++)
-				std::cout << std::toupper(str[j]) << std::endl;
+			std::cout << std::toupper(str[j]);
 		}
+		std::cout << std::endl;
 	}
 	return (0);
 }

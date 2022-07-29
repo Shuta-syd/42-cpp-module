6eac92f7f9aef8b27ce8410b382fe7f9b6a83bfd
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-29 09:26:22 +0900

    [add] megaphone.cpp

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,28 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 23:45:41 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/28 23:48:17 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/29 09:25:39 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int	main(int argc, char const *argv[])
+{
+	size_t	i;
+	size_t	j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			std::string str(argv[i]);
+			for (j = 0; j < str.length(); j++)
+				std::cout << std::toupper(str[j]) << std::endl;
+		}
+	}
+	return (0);
+}

4efb281513d50b7e73f3f738327fe8ad2f9a85b5
Shuta-syd <shuta.ogura0921@gmail.com>
2022-08-02 15:43:42 +0900

    [add] Harl private method added

--- cpp01/ex05/src/Harl.cpp
+++ cpp01/ex05/src/Harl.cpp
@@ -6,16 +6,45 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:24:55 by shogura           #+#    #+#             */
-/*   Updated: 2022/08/02 15:25:42 by shogura          ###   ########.fr       */
+/*   Updated: 2022/08/02 15:43:28 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Harl.hpp>
 
-Harl::Harl(/* args */)
+Harl::Harl()
 {
 }
 
 Harl::~Harl()
 {
+}
+
+void Harl::complain(std::string level)
+{
+	
+}
+
+void Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+			  << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+			  << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+			  << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now."
+			  << std::endl;
 }

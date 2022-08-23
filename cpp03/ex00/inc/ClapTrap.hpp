a6e743ad9076d19f636d91c4825622ba142b736d
Shuta-syd <shuta.ogura0921@gmail.com>
2022-08-23 17:16:38 +0900

    [mod] cpp03/ex00

--- cpp03/ex00/inc/ClapTrap.hpp
+++ cpp03/ex00/inc/ClapTrap.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   CLapTrap.hpp                                       :+:      :+:    :+:   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 10:51:56 by shogura           #+#    #+#             */
-/*   Updated: 2022/08/07 12:16:26 by shogura          ###   ########.fr       */
+/*   Updated: 2022/08/23 15:51:48 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,8 @@
 
 class ClapTrap
 {
-private:
-	std::string _name;
-	int _hp;
-	int _ep;
-	int _atk;
-
 public:
+	ClapTrap();
 	ClapTrap(std::string);
 	ClapTrap(const ClapTrap &);
 	~ClapTrap();
@@ -36,6 +31,12 @@ public:
 	void beRepaired(unsigned int amount);
 	void pause_action(void);
 	void die(void);
+
+private:
+	std::string _name;
+	int _hp;
+	int _ep;
+	int _atk;
 };
 
 #endif // ClapTrap_HPP
--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 10:51:24 by shogura           #+#    #+#             */
-/*   Updated: 2022/08/07 12:22:18 by shogura          ###   ########.fr       */
+/*   Updated: 2022/08/23 15:52:25 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int main(void)
 	claptrap.attack("nature");
 	claptrap.attack("nature");
 	claptrap.attack("nature");
+	claptrap.beRepaired(10);
+	claptrap.beRepaired(10);
 	claptrap.takeDamage(1000);
 	claptrap.attack("nature");
 	claptrap.attack("nature");
-	claptrap.beRepaired(10);
-	claptrap.beRepaired(10);
 	claptrap.beRepaired(10);
 	claptrap.beRepaired(10);
 

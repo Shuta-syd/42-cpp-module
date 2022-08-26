fcb00dd53599143ed76a13471dfa624b24366d5c
Shuta-syd <shuta.ogura0921@gmail.com>
2022-08-26 21:55:28 +0900

    [mod] ex00 main.c

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:34:07 by shogura           #+#    #+#             */
-/*   Updated: 2022/08/26 14:26:36 by shogura          ###   ########.fr       */
+/*   Updated: 2022/08/26 21:55:14 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		class TypeConvert type(argv[1]);
+		type.converter();
 	}
 	else
 		std::cerr << "[Usage] ./convert [numeric or character value]" << std::endl;
-
 	return 0;
 }

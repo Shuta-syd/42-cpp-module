e67d71f538bc0d54cef0c103130c19cc3fa833f0
Shuta-syd <shuta.ogura0921@gmail.com>
2022-08-27 18:14:33 +0900

    [fin] cpp06/ex01

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -1,17 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 22:29:10 by shogura           #+#    #+#             */
-/*   Updated: 2022/08/26 22:29:15 by shogura          ###   ########.fr       */
+/*   Updated: 2022/08/27 18:13:05 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char const *argv[])
+#include <string>
+#include <iostream>
+
+typedef struct s_data
 {
-	
+	int x_;
+	int y_;
+} Data;
+
+uintptr_t serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
+int main(void)
+{
+	Data d;
+	Data *ret;
+	uintptr_t i;
+
+	d.x_ = 4;
+	d.y_ = 2;
+	i = serialize(&d);
+	std::cout << i << std::endl;
+	ret = deserialize(i);
+	std::cout << ret << std::endl;
+	if (i == serialize(ret))
+		std::cout << "TRUE" << std::endl;
 	return 0;
 }
